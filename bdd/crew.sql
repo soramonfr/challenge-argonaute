26f355b1d94428d99145810bfd103512d1c3f3c7
sorayacruchet <soraya.cruchet@gmail.com>
2021-02-20 17:14:12 +0100

    script MySQL init

---
+++ bdd/crew.sql
@@ -0,0 +1,17 @@
+#------------------------------------------------------------
+#        Script MySQL.
+#------------------------------------------------------------
+CREATE DATABASE IF NOT EXISTS `crew` CHARACTER SET 'utf8';
+USE `crew`;
+
+#------------------------------------------------------------
+# Table: `crew_member`
+#------------------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS `crew_member`(
+        `id`        INT (11) AUTO_INCREMENT  NOT NULL ,
+        `firstname` VARCHAR (25) NOT NULL ,
+        `description`     VARCHAR (25) NOT NULL,
+        `gender`     VARCHAR (5) NOT NULL,
+        PRIMARY KEY (`id`)
+)ENGINE=InnoDB;

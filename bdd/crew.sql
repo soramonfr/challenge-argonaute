c3417bb33ea281e032a71cbe4776abe28cc81915
sorayacruchet <soraya.cruchet@gmail.com>
2021-02-20 17:22:49 +0100

    modig db: set up contrainte d'unicité (prénom + nom)

--- bdd/crew.sql
+++ bdd/crew.sql
@@ -9,9 +9,11 @@ USE `crew`;
 #------------------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS `crew_member`(
-        `id`        INT (11) AUTO_INCREMENT  NOT NULL ,
-        `firstname` VARCHAR (25) NOT NULL ,
+        `id`        INT (11) AUTO_INCREMENT  NOT NULL,
+        `firstname` VARCHAR (25) NOT NULL,
+        `firstname` VARCHAR (25) NOT NULL,
         `description`     VARCHAR (25) NOT NULL,
         `gender`     VARCHAR (5) NOT NULL,
         PRIMARY KEY (`id`)
+        UNIQUE KEY `firstname` (`firstname`,`lastname`)
 )ENGINE=InnoDB;
